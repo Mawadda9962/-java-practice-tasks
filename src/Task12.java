5749149479e5b2b6255d98c4e0d327c57a3af84b
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-07 12:35:04 +0400

    Solving Task12

---
+++ src/Task12.java
@@ -0,0 +1,19 @@
+public class Task12 {
+    public static void main(String[] args) {
+        System.out.println(sortaSum(3, 4));
+
+
+    }
+    public static int sortaSum(int a, int b) {
+        int sum = a + b;
+
+        if (sum >= 10 &&  sum <= 19) {
+            return 20;
+        } else {
+            return sum;
+        }
+    }
+
+}
+
+
