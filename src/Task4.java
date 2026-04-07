718a5463483332b575aa652d888710023e837d4e
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-07 11:10:11 +0400

    Solving Task4

---
+++ src/Task4.java
@@ -0,0 +1,16 @@
+public class Task4 {
+    public static void main(String[] args) {
+        System.out.println(more20(20));
+
+
+    }
+    public static boolean more20(int n) {
+        if ((n % 2 == 1) || (n % 2 == 2)) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+
